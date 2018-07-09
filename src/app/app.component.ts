e560fb7523bb4b7ea49ac82d6264bc567b7ed26c
Vova Novak <novak@step.itstep.rv.ua>
2018-07-09 20:56:41 +0300

    Lesson 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<question></question>'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'my app';
 }
